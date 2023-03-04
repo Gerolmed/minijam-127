357ab99c6f3c98fe05eaf2b78246e2fb973d06bc
CreepyPvP <interruptedThread@gmail.com>
2023-03-04 18:45:33 +0100

    Enemies now follow the player

--- src/entities/living/Enemies/Wolf.ts
+++ src/entities/living/Enemies/Wolf.ts
@@ -1,7 +1,11 @@
 import {Enemy} from "./Enemy";
+import Vector2 = Phaser.Math.Vector2;
 
 export class Wolf extends Enemy {
 
+
+    private readonly SPEED = 50;
+    private readonly RANGE = 50;
 
     create() {
         super.create();
@@ -9,7 +13,22 @@ export class Wolf extends Enemy {
 
     protected safeUpdate(deltaTime: number) {
         super.safeUpdate(deltaTime);
-        console.log(this.hasLineOfSight());
+
+        if(this.hasLineOfSight()) {
+            const player = this.physicsSocket.getPlayer()!;
+
+            const playerDir = [player.x - this.x, player.y - this.y];
+            const length = Math.sqrt(playerDir[0] * playerDir[0] + playerDir[1] * playerDir[1]);
+
+            if(length <= this.RANGE)
+                return;
+
+            this.scene.matter.setVelocity(
+                this.rigidbody,
+                playerDir[0] / length * this.SPEED * deltaTime,
+                playerDir[1] / length * this.SPEED * deltaTime
+            )
+        }
     }
 
 }
