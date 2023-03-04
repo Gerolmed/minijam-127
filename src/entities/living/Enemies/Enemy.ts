b454078e5614e4a00a328ee405bf67ce52c94c0e
CreepyPvP <interruptedThread@gmail.com>
2023-03-05 00:52:53 +0100

    Fixed enemy rotation + moving

--- src/entities/living/Enemies/Enemy.ts
+++ src/entities/living/Enemies/Enemy.ts
@@ -6,6 +6,7 @@ import GameScene from "../../../scenes/Game";
 import PhysicsLayers from "../../PhysicsLayers";
 import {ProjectileShooter} from "../../projectiles/shooting/ProjectileShooter";
 import {EnemyHealthBar} from "../../../ui/EnemyHealthBar";
+import {Animator} from "../../../animations/Animator";
 
 
 export enum EnemyState {
@@ -14,16 +15,41 @@ export enum EnemyState {
     RETREATING
 }
 
+export enum EnemyFacing {
+    LEFT,
+    RIGHT,
+    TOP,
+    DOWN
+}
+
 export class Enemy extends LivingEntity {
 
 
     protected readonly projectileShooter: ProjectileShooter;
+
+
+    private readonly SPEED = 52;
+    private readonly RETREAT_SPEED = 25;
+    private readonly ATTACK_RANGE = 50;
+    private readonly AGGRO_RANGE = 150;
+    private readonly AGGRO_RANGE_ORIGIN = 100;
+    private readonly PATIENCE = 3 * 1000;
+    private readonly FOLLOW_DISTANCE = 200;
+    private readonly RETREAT_DISTANCE_MAX = 250;
+    private readonly RETREAT_DURATION_MAX = 10 * 1000;
+
+    private lastPlayerSpotted = Date.now();
+    private startRetreating = 0;
+    private aState: EnemyState = EnemyState.NEUTRAL;
+    private facing: EnemyFacing = EnemyFacing.DOWN;
+
 
     constructor(scene: GameScene,
                 protected readonly physicsSocket: PhysicsSocket,
                 protected readonly origin: Vector2) {
         super(scene, origin.x, origin.y);
 
+        this.facing = EnemyFacing.DOWN;
         this.projectileShooter = new ProjectileShooter(scene, this);
     }
 
@@ -33,6 +59,89 @@ export class Enemy extends LivingEntity {
 
         this.projectileShooter.update(deltaTime);
     }
+
+    protected move(deltaTime: number, animator: Animator) {
+        super.safeUpdate(deltaTime);
+
+        const player = this.physicsSocket.getPlayer();
+        if(!player) return;
+
+        const hasLos = this.hasLineOfSight();
+        const playerDir = [player.x - this.x, player.y - this.y];
+        const distanceToPlayer = Math.sqrt(playerDir[0] * playerDir[0] + playerDir[1] * playerDir[1]);
+        const distanceToOrigin = Math.sqrt((this.x - this.origin.x) * (this.x - this.origin.x) + (this.y - this.origin.y) * (this.y - this.origin.y));
+        const timeLastSpotted = Date.now() - this.lastPlayerSpotted;
+        let moving = false;
+
+        if(hasLos)
+            this.lastPlayerSpotted = Date.now();
+
+        if(this.aState === EnemyState.AGGRO && (distanceToOrigin> this.FOLLOW_DISTANCE || timeLastSpotted > this.PATIENCE)) {
+            this.aState = EnemyState.RETREATING;
+            this.startRetreating = Date.now();
+        }
+        const timeRetreating = Date.now() - this.startRetreating;
+
+        if(this.aState !== EnemyState.AGGRO && distanceToPlayer < this.AGGRO_RANGE && distanceToOrigin < this.AGGRO_RANGE_ORIGIN)
+            this.aState = EnemyState.AGGRO;
+
+        if(this.aState === EnemyState.AGGRO && hasLos && distanceToPlayer > this.ATTACK_RANGE) {
+            const velX = playerDir[0] / distanceToPlayer * this.SPEED * deltaTime;
+            const velY = playerDir[1] / distanceToPlayer * this.SPEED * deltaTime;
+            this.scene.matter.setVelocity(
+                this.rigidbody,
+                velX,
+                velY
+            )
+
+            moving = true;
+            this.updateFacing(velX, velY);
+        }
+
+        if(this.aState === EnemyState.RETREATING) {
+            const velX = -playerDir[0] / distanceToPlayer * this.RETREAT_SPEED * deltaTime;
+            const velY = -playerDir[1] / distanceToPlayer * this.RETREAT_SPEED * deltaTime;
+            this.scene.matter.setVelocity(
+                this.rigidbody,
+                velX,
+                velY
+            )
+
+            moving = true;
+            this.updateFacing(velX, velY);
+        }
+        if(this.aState === EnemyState.RETREATING && (distanceToOrigin > this.RETREAT_DISTANCE_MAX || timeRetreating > this.RETREAT_DURATION_MAX)) {
+            this.aState = EnemyState.NEUTRAL;
+        }
+
+        if(this.aState === EnemyState.NEUTRAL) {
+            this.rigidbody.position.x = this.origin.x;
+            this.rigidbody.position.y = this.origin.y;
+
+            this.facing = EnemyFacing.DOWN;
+        }
+
+        this.animator.play(this.getAnimationFrame(this.facing, moving? "WALK" : "IDLE"));
+    }
+
+    private updateFacing(velX: number, velY: number) {
+        const horizontal = Math.abs(velX) > Math.abs(velY);
+
+        if(horizontal) {
+            if(velX < 0) {
+                this.facing = EnemyFacing.LEFT;
+            } else {
+                this.facing = EnemyFacing.RIGHT;
+            }
+        } else {
+            if(velY < 0) {
+                this.facing = EnemyFacing.TOP;
+            } else {
+                this.facing = EnemyFacing.DOWN;
+            }
+        }
+    }
+
     create() {
         super.create();
         const healthBar = new EnemyHealthBar(this.scene, this.getHealthBarOffset())
@@ -62,6 +171,11 @@ export class Enemy extends LivingEntity {
 
     protected getPhysicsLayer(): number {
         return PhysicsLayers.ENEMY;
+    }
+
+
+    getAnimationFrame(facing: EnemyFacing, animation: "WALK" | "IDLE"): string {
+        return "";
     }
 
 }
--- src/entities/living/Enemies/Wolf.ts
+++ src/entities/living/Enemies/Wolf.ts
@@ -1,75 +1,38 @@
-import {Enemy, EnemyState} from "./Enemy";
+import {Enemy, EnemyFacing} from "./Enemy";
+import {WolfAnimationKeys} from "../../../animations/EnemyAnimationKeys";
+import Vector2 = Phaser.Math.Vector2;
 
 
 export class Wolf extends Enemy {
 
 
-    private readonly SPEED = 52;
-    private readonly RETREAT_SPEED = 25;
-    private readonly ATTACK_RANGE = 50;
-    private readonly AGGRO_RANGE = 150;
-    private readonly AGGRO_RANGE_ORIGIN = 100;
-    private readonly PATIENCE = 3 * 1000;
-    private readonly FOLLOW_DISTANCE = 200;
-    private readonly RETREAT_DISTANCE_MAX = 250;
-    private readonly RETREAT_DURATION_MAX = 10 * 1000;
-
-    private lastPlayerSpotted = Date.now();
-    private startRetreating = 0;
-    private aState: EnemyState = EnemyState.NEUTRAL;
-
     create() {
         super.create();
+
+        this.animator.load(WolfAnimationKeys.BASE);
+        this.animator.play(WolfAnimationKeys.IDLE_DOWN);
+
+        this.physicsOffset = new Vector2(-1,-2);
     }
 
     protected safeUpdate(deltaTime: number) {
         super.safeUpdate(deltaTime);
 
-        const player = this.physicsSocket.getPlayer();
-        if(!player) return;
+        this.move(deltaTime, this.animator);
+    }
 
-        const hasLos = this.hasLineOfSight();
-        const playerDir = [player.x - this.x, player.y - this.y];
-        const distanceToPlayer = Math.sqrt(playerDir[0] * playerDir[0] + playerDir[1] * playerDir[1]);
-        const distanceToOrigin = Math.sqrt((this.x - this.origin.x) * (this.x - this.origin.x) + (this.y - this.origin.y) * (this.y - this.origin.y));
-        const timeLastSpotted = Date.now() - this.lastPlayerSpotted;
-
-        if(hasLos)
-            this.lastPlayerSpotted = Date.now();
-
-        if(this.aState === EnemyState.AGGRO && (distanceToOrigin> this.FOLLOW_DISTANCE || timeLastSpotted > this.PATIENCE)) {
-            this.aState = EnemyState.RETREATING;
-            this.startRetreating = Date.now();
-        }
-        const timeRetreating = Date.now() - this.startRetreating;
-
-        if(this.aState !== EnemyState.AGGRO && distanceToPlayer < this.AGGRO_RANGE && distanceToOrigin < this.AGGRO_RANGE_ORIGIN)
-            this.aState = EnemyState.AGGRO;
-
-        if(this.aState === EnemyState.AGGRO && hasLos && distanceToPlayer > this.ATTACK_RANGE) {
-            this.scene.matter.setVelocity(
-                this.rigidbody,
-                playerDir[0] / distanceToPlayer * this.SPEED * deltaTime,
-                playerDir[1] / distanceToPlayer * this.SPEED * deltaTime
-            )
+    getAnimationFrame(facing: EnemyFacing, animation: "WALK" | "IDLE"): string {
+        if(facing === EnemyFacing.LEFT) {
+            return animation === "WALK" ? WolfAnimationKeys.WALK_LEFT : WolfAnimationKeys.IDLE_LEFT;
+        } else if(facing === EnemyFacing.DOWN) {
+            return animation === "WALK" ? WolfAnimationKeys.WALK_DOWN : WolfAnimationKeys.IDLE_DOWN;
+        } else if(facing === EnemyFacing.RIGHT) {
+            return animation === "WALK" ? WolfAnimationKeys.WALK_RIGHT : WolfAnimationKeys.IDLE_RIGHT;
+        } else if(facing === EnemyFacing.TOP) {
+            return animation === "WALK" ? WolfAnimationKeys.WALK_UP : WolfAnimationKeys.IDLE_UP;
         }
 
-        if(this.aState === EnemyState.RETREATING) {
-            this.scene.matter.setVelocity(
-                this.rigidbody,
-                -playerDir[0] / distanceToPlayer * this.RETREAT_SPEED * deltaTime,
-                -playerDir[1] / distanceToPlayer * this.RETREAT_SPEED * deltaTime
-            )
-        }
-        if(this.aState === EnemyState.RETREATING && (distanceToOrigin > this.RETREAT_DISTANCE_MAX || timeRetreating > this.RETREAT_DURATION_MAX)) {
-            this.aState = EnemyState.NEUTRAL;
-        }
-
-        if(this.aState === EnemyState.NEUTRAL) {
-            this.rigidbody.position.x = this.origin.x;
-            this.rigidbody.position.y = this.origin.y;
-        }
-
+        throw new Error("Invalid animation frame");
     }
 
 }
