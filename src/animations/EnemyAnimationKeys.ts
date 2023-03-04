38d2d05d4e930e7614ff60a93c0a0f930f4d9612
Gerolmed <werwolf420@yahoo.de>
2023-03-04 21:56:16 +0100

    Added enemy animation keys

---
+++ src/animations/EnemyAnimationKeys.ts
@@ -0,0 +1,36 @@
+
+export const RatAnimationKeys = Object.freeze({
+    BASE: "Rat",
+    IDLE_LEFT: "Left_Idle",
+    IDLE_RIGHT: "Right_Idle",
+    IDLE_UP: "Back_Idle",
+    IDLE_DOWN: "Front_Idle",
+    WALK_LEFT: "Left_Walk",
+    WALK_RIGHT: "Right_Walk",
+    WALK_UP: "Back_Walk",
+    WALK_DOWN: "Front_Walk",
+});
+
+export const OpossumAnimationKeys = Object.freeze({
+    BASE: "Opossum",
+    IDLE_LEFT: "Left_Idle",
+    IDLE_RIGHT: "Right_Idle",
+    IDLE_UP: "Back_Idle",
+    IDLE_DOWN: "Front_Idle",
+    WALK_LEFT: "Left_Walk",
+    WALK_RIGHT: "Right_Walk",
+    WALK_UP: "Back_Walk",
+    WALK_DOWN: "Front_Walk",
+});
+
+export const WolfAnimationKeys = Object.freeze({
+    BASE: "Wolf",
+    IDLE_LEFT: "Left_Idle",
+    IDLE_RIGHT: "Right_Idle",
+    IDLE_UP: "Back_Idle",
+    IDLE_DOWN: "Front_Idle",
+    WALK_LEFT: "Left_Walk",
+    WALK_RIGHT: "Right_Walk",
+    WALK_UP: "Back_Walk",
+    WALK_DOWN: "Front_Walk",
+});
