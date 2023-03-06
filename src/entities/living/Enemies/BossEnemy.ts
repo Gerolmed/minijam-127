2c582ec37b79a36565f6c4730f01c4ac39d5d1b0
CreepyPvP <interruptedThread@gmail.com>
2023-03-06 04:59:21 +0100

    Added ratking animations

--- src/entities/living/Enemies/BossEnemy.ts
+++ src/entities/living/Enemies/BossEnemy.ts
@@ -5,11 +5,15 @@ import Vector2 = Phaser.Math.Vector2;
 import {AABB} from "../../../util/AABB";
 import {Jukebox} from "../../../audio/JukeBox";
 import {BehaviourStateMachine} from "../../../behaviour/BehaviourStateMachine";
+import {BehaviourBuilder} from "../../../behaviour/BehaviourBuilder";
+import {CooldownManager} from "../../../behaviour/CooldownManager";
 
 export class BossEnemy extends Enemy {
 
 
     private isInCombat = false;
+
+    private readonly cooldownManager: CooldownManager;
 
     constructor(scene: GameScene,
                 physicsSocket: PhysicsSocket,
@@ -18,6 +22,9 @@ export class BossEnemy extends Enemy {
                 private readonly arenaSize: Vector2) {
         super(scene, physicsSocket, origin);
 
+        const abilities = new Map<string, number>();
+        abilities.set("bulletHell", 3 * 1000);
+        this.cooldownManager = new CooldownManager(abilities);
     }
 
     protected safeUpdate(deltaTime: number) {
@@ -29,6 +36,28 @@ export class BossEnemy extends Enemy {
 
         if(!this.isInCombat && AABB.isIn(player.x, player.y, this.arenaTopLeft.x, this.arenaTopLeft.y, this.arenaSize.x, this.arenaSize.y))
             this.startBossFight();
+    }
+
+
+    protected createBehaviour(): BehaviourStateMachine<EnemyAiParams> {
+        return new BehaviourBuilder<EnemyAiParams>()
+            .addState("neutral")
+                .onUpdate((param) => this.setToOrigin())
+                .and()
+            .addState("aggro")
+                .onUpdate((param, delta) => this.moveIntoAttackRange(param, delta))
+                .and()
+            // .addState("bulletHell")
+            //    .onUpdate((param, delta) => this.doBulletHell(param, delta))
+            //    .and()
+            .setStart("neutral")
+                .addTransition("aggro", (param, delta) => this.shouldAggro(param, delta))
+                .setDataProvider(() => this.getAiParams())
+            .build()
+    }
+
+    protected doBulletHell() {
+
     }
 
     private startBossFight() {
--- src/entities/living/Enemies/RatKing.ts
+++ src/entities/living/Enemies/RatKing.ts
@@ -1,5 +1,35 @@
 import {BossEnemy} from "./BossEnemy";
+import {EnemyAiParams, EnemyFacing} from "./Enemy";
+import {RatKingAnimationKeys, WolfAnimationKeys} from "../../../animations/EnemyAnimationKeys";
+import Vector2 = Phaser.Math.Vector2;
+import {BehaviourStateMachine} from "../../../behaviour/BehaviourStateMachine";
+import {BehaviourBuilder} from "../../../behaviour/BehaviourBuilder";
 
 export class RatKing extends BossEnemy {
+
+    create() {
+        super.create();
+
+        this.animator.load(RatKingAnimationKeys.BASE);
+        this.animator.play(RatKingAnimationKeys.IDLE_DOWN);
+
+        this.setupHealth(200);
+
+        this.physicsOffset = new Vector2(-1,-2);
+    }
+
+    getAnimationFrame(facing: EnemyFacing, animation: "WALK" | "IDLE"): string {
+        if(facing === EnemyFacing.RIGHT)  {
+            return animation === "WALK"? RatKingAnimationKeys.WALK_RIGHT: RatKingAnimationKeys.IDLE_RIGHT;
+        } else if(facing === EnemyFacing.LEFT)  {
+            return animation === "WALK"? RatKingAnimationKeys.WALK_LEFT: RatKingAnimationKeys.IDLE_LEFT;
+        } else if(facing === EnemyFacing.TOP)  {
+            return animation === "WALK"? RatKingAnimationKeys.WALK_UP: RatKingAnimationKeys.IDLE_UP;
+        } else if(facing === EnemyFacing.DOWN)  {
+            return animation === "WALK"? RatKingAnimationKeys.WALK_DOWN: RatKingAnimationKeys.IDLE_DOWN;
+        }
+
+        throw new Error("Invalid animation frame")
+    }
 
 }
